7095c8225f19bb44d0bd93d5d539901bd5cc284d
Quoc Bao Code <baoquoczero@gmail.com>
2023-09-26 22:04:29 +0700

    up

---
+++ Bảng cửu chương/Bảng cửu chương/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bảng_cửu_chương
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (j == 10)
+                    {
+                        Console.WriteLine("{0} x {1}={2}", i, j, i * j);
+                    }else
+                        if (i* j >= 10)
+                    {
+                        Console.WriteLine("{0} x {1} ={2}", i, j, i * j);
+                    }else
+                        Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
